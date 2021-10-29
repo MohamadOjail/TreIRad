952c3d1f54691ae4b10dcdd1be4b91bddff4b3f2
Mohamad Ojail <mohamad.ojail@yaelev.se>
2021-10-30 00:07:42 +0200

    miner fixes

--- src/game/tools/GameLogic.java
+++ src/game/tools/GameLogic.java
@@ -17,7 +17,7 @@ public class GameLogic {
 	TakeInput input = new TakeInput(this);
 
 	public void run() {
-		MSG.welcome(50);
+		MSG.welcome(80);
 		
 		play();
 		
--- src/game/tools/PrintOut.java
+++ src/game/tools/PrintOut.java
@@ -18,29 +18,42 @@ public class PrintOut {
 			for (int j = 0; j < 2; j++) {
 				System.out.print("   |  ");
 			}
+			
 
 			System.out.println();
 			System.out.print("\t");
+			
+			
 
 			for (int j = 0; j < board[i].length; j++) {
 				print = (j < 2) ? board[i][j] + "  |  " : board[i][j];
 				System.out.print(print);
 			}
+			
+			
 
 			System.out.println();
 			System.out.print("\t");
+			
+			
 
 			for (int j = 0; j < 2; j++) {
 				System.out.print("   |  ");
 			}
+			
+			
 
 			System.out.println();
 			System.out.print("    ");
+			
+			
 
 			for (int j = 0; j < board[i].length; j++) {
 				if (i < 2)
 					System.out.print("-------");
 			}
+			
+			
 
 			System.out.println();
 		}
